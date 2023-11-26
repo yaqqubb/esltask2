d5a9d137353e98bbf606d6885963b50cc88673a6
yaqqubb <yagubma@code.edu.az>
2023-11-26 12:26:21 +0400

    first commit

---
+++ task222/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Authentication;
+
+namespace task222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1 ");
+            string name = "Yaqub";
+            string surname = "Ehmedov";
+            Console.WriteLine(name + " " + surname);
+            Console.WriteLine(" "); Console.WriteLine(" ");
+
+
+            Console.WriteLine("Task 2 ");
+            int a = 7 ; int b = 7;
+            Console.WriteLine(a + b);
+            Console.WriteLine(" "); Console.WriteLine(" "); 
+
+            Console.WriteLine("Task 3 ");
+            int a1 = 20; int b1 = 50;
+            Console.WriteLine(b1 - a1);
+            Console.WriteLine(" "); Console.WriteLine(" ");
+
+            Console.WriteLine("Task 4 ");
+            int a3 = 5; int b3 = 7; int c = a3 * b3;
+            Console.WriteLine(c); Console.WriteLine(c * c);
+            Console.WriteLine(" "); Console.WriteLine(" ");
+
+
+            Console.WriteLine("Task 5 ");
+            int a4 = 10; int b4 = 2; int c2 = a4 / b4;
+            Console.WriteLine(c2); Console.WriteLine(c2 * c2);  
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
